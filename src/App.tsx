b185bc18f9c6a1e08642431c7f44d043d1140531
Justin Haddock <jhadddock@protonmail.com>
2024-11-08 20:50:16 -0800

    Add test flashcard to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { WordData } from './wordDataDefinitions';
+import Flashcard from './components/Flashcard';
+
+const sampleWordData: WordData = {
+  word: 'computer',
+  partOfSpeech: 'noun (countable)',
+  meaning: new Map([['en', "комп'ютер"]]),
+  pronunciation: 'kəmˈpju·tə̬r',
+  definition:
+    'An electronic machine that can store and arrange large amounts of information.',
+  exampleSentences: [
+    'I use my computer to write emails.',
+    'She has a new computer at home.',
+    'There are lots of computers at the library.',
+  ],
+  exampleSentencesTranslations: new Map([
+    [
+      'en',
+      [
+        'Я використовую свій комп’ютер для написання електронних листів.',
+        'У неї вдома новий комп’ютер.',
+        'У бібліотеці багато комп’ютерів.',
+      ],
+    ],
+  ]),
+  icon: 'src/assets/icons/computer.svg',
+  images: ['src/assets/images/computer.jpg', 'src/assets/images/computer2.jpg'],
+  usageNotes: new Map(),
+  usageNotesEnglish: {
+    synonyms: ['PC', 'laptop'],
+    antonyms: [],
+  },
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Flashcard wordData={sampleWordData} />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +63,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
