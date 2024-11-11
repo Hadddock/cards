fae95eb2e96230ecde4e62f0200aa44fa2edd48e
Justin Haddock <jhadddock@protonmail.com>
2024-11-10 18:54:23 -0800

    Add Deck component and refactor Flashcard

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -1,70 +1,69 @@
 import React from 'react';
 import './../css/Flashcard.css';
-import { WordData } from '../wordDataDefinitions';
 
-// Adjusted FlashcardProps to accept only a single wordData prop
+interface Flashcard {
+  word: string;
+  partOfSpeech: string | string[];
+  level?: string;
+  definition?: string;
+  pronunciation?: string;
+  meaning?: { [key: string]: string };
+  exampleSentences?: string[];
+  exampleSentencesTranslations?: { [key: string]: string[] };
+}
+
 interface FlashcardProps {
-  wordData: WordData;
+  wordData: Flashcard;
 }
 
 const Flashcard: React.FC<FlashcardProps> = ({ wordData }) => {
-  const {
-    word,
-    partOfSpeech,
-    meaning,
-    pronunciation,
-    definition,
-    exampleSentences,
-    exampleSentencesTranslations,
-  } = wordData;
-
   return (
     <div className="flashcard">
-      {/* {icon && <img src={icon} alt={`${word} icon`} className="icon" />} */}
-      <h1 className="word">{word}</h1>
-      <p className="partOfSpeech">{partOfSpeech}</p>
+      {wordData.word && <h1 className="word">{wordData.word}</h1>}
+      {wordData.partOfSpeech && (
+        <p className="partOfSpeech">
+          {Array.isArray(wordData.partOfSpeech)
+            ? wordData.partOfSpeech.join(', ')
+            : wordData.partOfSpeech}
+        </p>
+      )}
 
-      <section className="meaning">
-        <h2>Meaning</h2>
-        <p>{meaning.get('en') || ''}</p> {/* Defaulting to English meaning */}
-      </section>
+      {wordData.meaning?.['en'] && (
+        <section className="meaning">
+          <h2>Meaning</h2>
+          <p>{wordData.meaning['en']}</p> {/* Defaulting to English meaning */}
+        </section>
+      )}
 
-      <section className="pronunciation">
-        <h2>Pronunciation</h2>
-        <p>{pronunciation}</p>
-      </section>
+      {wordData.pronunciation && (
+        <section className="pronunciation">
+          <h2>Pronunciation</h2>
+          <p>{wordData.pronunciation}</p>
+        </section>
+      )}
 
-      <section className="definition">
-        <h2>Definition</h2>
-        <p>{definition}</p>
-      </section>
+      {wordData.definition && (
+        <section className="definition">
+          <h2>Definition</h2>
+          <p>{wordData.definition}</p>
+        </section>
+      )}
 
-      <section className="examples">
-        <h2>Example Sentences</h2>
-        <ul>
-          {exampleSentences.map((example, index) => (
-            <li key={index}>
-              {example}
-              <p className="translation">
-                {exampleSentencesTranslations.get('en')?.[index] || ''}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </section>
-      {/* <section className="images">
-        <h2>Images</h2>
-        <div className="image-gallery">
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`${word} image ${index + 1}`}
-              className="image"
-            />
-          ))}
-        </div>
-      </section> */}
+      {wordData.exampleSentences && wordData.exampleSentences?.length > 0 && (
+        <section className="examples">
+          <h2>Example Sentences</h2>
+          <ul>
+            {wordData.exampleSentences.map((example, index) => (
+              <li key={index}>
+                {example}
+                <p className="translation">
+                  {wordData.exampleSentencesTranslations?.['en']?.[index] || ''}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
     </div>
   );
 };
